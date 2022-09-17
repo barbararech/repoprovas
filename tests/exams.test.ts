6d2c8ed6ef61ca3376c275bc1abfe577b23b965a
barbararech <rech.brb@gmail.com>
2022-09-17 20:15:19 -0300

    test: add get exams by teachers test

--- tests/exams.test.ts
+++ tests/exams.test.ts
@@ -176,7 +176,41 @@ describe("Test GET /tests/discipline", () => {
   });
 });
 
+describe("Test GET /tests/teachers", () => {
+    it("Should return 200 if get tests correctly", async () => {
+      const user = await userSignUpFactory();
+      await supertest(app).post(`/signup`).send(user);
+  
+      const userLogin = { email: user.email, password: user.password };
+      const response = await supertest(app).post(`/signin`).send(userLogin);
+      const { token } = response.body;
+  
+      const result = await supertest(app)
+        .get(`/tests/teachers`)
+        .auth(token, { type: "bearer" });
 
+      expect(result.status).toBe(200);
+      expect(result.body).toBeInstanceOf(Object);
+    });
+  
+    it("Should return 500 if send invalid token", async () => {
+      const user = await userSignUpFactory();
+      await supertest(app).post(`/signup`).send(user);
+  
+      const userLogin = { email: user.email, password: user.password };
+      const response = await supertest(app).post(`/signin`).send(userLogin);
+      let { token } = response.body;
+      token = token + "a";
+  
+      const test = await testFactory();
+      const result = await supertest(app)
+        .get(`/tests/teachers`)
+        .send(test)
+        .auth(token, { type: "bearer" });
+  
+      expect(result.status).toBe(500);
+    });
+  });
 
 afterAll(async () => {
   await client.$disconnect();
